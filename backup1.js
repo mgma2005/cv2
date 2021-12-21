b76945295e009845a1a1a6cba6775c5cbfebab93
mgma2005 <95400812+mgma2005@users.noreply.github.com>
2021-12-21 01:43:46 -0300

    Add files via upload

---
+++ backup1.js
@@ -0,0 +1,178 @@
+//variable boton
+var checkbox = document.getElementById('switch-label');
+
+   
+checkbox.addEventListener( 'change', function() {
+  if(!this.checked) {
+     location.reload();
+  }});
+
+checkbox.addEventListener( 'change', function() {
+    if(this.checked) {
+
+      //elemento background color        
+        document.body.style.backgroundColor="yellow",
+        document.body.style.color="green"
+      
+
+      //elemento efecto letras d personales
+
+        var words = ['cool', 'dev', 'nerd', 'gratis', 'coder'],
+    wordWrapper = document.getElementById('word'),
+    wordWrapperContent = wordWrapper.innerHTML,
+    addingWord = false,
+    counter = 1;
+
+setInterval(function(){
+
+  if(wordWrapperContent.length > 0 && !addingWord ) {
+    wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
+    wordWrapperContent = wordWrapper.innerHTML;
+  } else {
+    addingWord = true;
+  }
+
+  if( addingWord ){
+    if( wordWrapperContent.length < words[counter].length  ) {
+      wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
+      wordWrapperContent = wordWrapper.innerHTML;
+    } else {
+      if( counter < words.length) {
+        counter ++
+      }
+      addingWord = false;
+    }
+  }
+
+  if( counter == words.length) {
+    counter = 0;
+  }
+
+}, 150);
+    }})
+    
+// letras exp laboral
+
+
+checkbox.addEventListener( 'change', function() {
+  if(this.checked) {
+
+    var words = ['cool', 'dev', 'nerd', 'gratis', 'coder'],
+    wordWrapper = document.getElementById('et'),
+    wordWrapperContent = wordWrapper.innerHTML,
+    addingWord = false,
+    counter = 1;
+
+setInterval(function(){
+
+  if(wordWrapperContent.length > 0 && !addingWord ) {
+    wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
+    wordWrapperContent = wordWrapper.innerHTML;
+  } else {
+    addingWord = true;
+  }
+
+  if( addingWord ){
+    if( wordWrapperContent.length < words[counter].length  ) {
+      wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
+      wordWrapperContent = wordWrapper.innerHTML;
+    } else {
+      if( counter < words.length) {
+        counter ++
+      }
+      addingWord = false;
+    }
+  }
+
+  if( counter == words.length) {
+    counter = 0;
+  }
+
+}, 150);
+
+    }});
+  
+  
+//letras educacion
+
+
+checkbox.addEventListener( 'change', function() {
+  if(this.checked) {
+
+    var words = ['cool', 'dev', 'nerd', 'gratis', 'coder'],
+    wordWrapper = document.getElementById('educ'),
+    wordWrapperContent = wordWrapper.innerHTML,
+    addingWord = false,
+    counter = 1;
+
+setInterval(function(){
+
+  if(wordWrapperContent.length > 0 && !addingWord ) {
+    wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
+    wordWrapperContent = wordWrapper.innerHTML;
+  } else {
+    addingWord = true;
+  }
+
+  if( addingWord ){
+    if( wordWrapperContent.length < words[counter].length  ) {
+      wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
+      wordWrapperContent = wordWrapper.innerHTML;
+    } else {
+      if( counter < words.length) {
+        counter ++
+      }
+      addingWord = false;
+    }
+  }
+
+  if( counter == words.length) {
+    counter = 0;
+  }
+
+}, 150);
+
+    }});
+  
+    
+    
+    //letras habilidades
+    
+checkbox.addEventListener( 'change', function() {
+  if(this.checked) {
+
+    var words = ['cool', 'dev', 'nerd', 'gratis', 'coder'],
+    wordWrapper = document.getElementById('habi'),
+    wordWrapperContent = wordWrapper.innerHTML,
+    addingWord = false,
+    counter = 1;
+
+setInterval(function(){
+
+  if(wordWrapperContent.length > 0 && !addingWord ) {
+    wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
+    wordWrapperContent = wordWrapper.innerHTML;
+  } else {
+    addingWord = true;
+  }
+
+  if( addingWord ){
+    if( wordWrapperContent.length < words[counter].length  ) {
+      wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
+      wordWrapperContent = wordWrapper.innerHTML;
+    } else {
+      if( counter < words.length) {
+        counter ++
+      }
+      addingWord = false;
+    }
+  }
+
+  if( counter == words.length) {
+    counter = 0;
+  }
+
+}, 150);
+
+    }});
+  
